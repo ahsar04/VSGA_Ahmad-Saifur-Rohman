5b3aba4a4468fb1d7c1c083b36918193be8f24cb
ahsar04 <asrsaif04@gmail.com>
2021-08-03 17:33:55 +0700

    update

--- VsgaLayout/app/src/main/java/com/asr/vsgalayout/LoginPage.java
+++ VsgaLayout/app/src/main/java/com/asr/vsgalayout/LoginPage.java
@@ -13,11 +13,4 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.optionsmenu,menu);
-        return true;
-    }
 }
